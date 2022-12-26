bdceeb2c11cf228d97c67105586590e83dab8bd2
Fatih Arslan <fatih1122335@gmail.com>
2022-12-26 20:27:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = new Random().Next(0, 100);
+            int tahmin=0;
+            do
+            {
+                Console.WriteLine("tuttuğum sayıyı tahmin et.");
+                tahmin = int.Parse(Console.ReadLine());
+                if (tahmin>sayi)
+                {
+                    Console.WriteLine("aşağı in");
+                }
+                else if (tahmin<sayi)
+                {
+                    Console.WriteLine("yukarı çık");
+                }
+            } while (tahmin!=sayi);
+            Console.WriteLine("tebrikler!");
+            Console.ReadKey();
+        }
+    }
+}
